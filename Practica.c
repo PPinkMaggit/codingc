b68ab1e2c07fee2badc7be754db4f5c3179a0106
PPinkMaggit <111446223+PPinkMaggit@users.noreply.github.com>
2023-06-21 15:16:25 -0300

    Update Practica.c

--- Practica.c
+++ Practica.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-
+//DEFINICIONES DE LAS FUNCIONES Y PROCEDIMIENTOS
  void mostrarGenerico(int v[], int t);
  int aleatorioGenerico(int min, int max);
+
 
 
 int main(){
 
 };
+//IMPLEMENTACIONES DE LAS FUNCIONES Y PROCEDIMIENTOS
  void mostrarGenerico(int v[], int t){
       printf("|| ");
     for ( int i = 0; i< t; i++){
